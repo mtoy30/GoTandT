7114cf5e3a416072f76b161c3725a4751fc8fbaa
mtoy30 <mtoy30@gmail.com>
2025-05-07 15:23:07 -0400

    Added Sandbox

--- DD_Buttons_Admin.user.js
+++ DD_Buttons_Admin.user.js
@@ -6,6 +6,7 @@
 // @downloadURL  https://raw.githubusercontent.com/mtoy30/GoTandT/main/DD_Buttons_Admin.user.js
 // @description  Custom script for Dynamics 365 CRM page with multiple button functionalities
 // @match        https://gotandt.crm.dynamics.com/*
+// @match        https://gttqap2.crm.dynamics.com/
 // @author        Michael Toy
 // @grant        GM_setClipboard
 // @grant        GM_registerMenuCommand
