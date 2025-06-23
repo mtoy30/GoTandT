bd5d217988ea1fe10a753554bf9d5e1584f9e2bd
mtoy30 <mtoy30@gmail.com>
2025-06-23 11:26:21 -0400

    Update DD_Buttons_Admin.user.js

--- DD_Buttons_Admin.user.js
+++ DD_Buttons_Admin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DD_Buttons_Admin
 // @namespace    https://github.com/mtoy30/GoTandT
-// @version      3.8.2
+// @version      3.8.3
 // @updateURL    https://raw.githubusercontent.com/mtoy30/GoTandT/main/DD_Buttons_Admin.user.js
 // @downloadURL  https://raw.githubusercontent.com/mtoy30/GoTandT/main/DD_Buttons_Admin.user.js
 // @description  Custom script for Dynamics 365 CRM page with multiple button functionalities
@@ -794,6 +794,7 @@ rows.forEach(row => {
             product !== "Wheelchair Rental" &&
             product !== "Airport Pickup Fee" &&
             product !== "After Hours Fee"
+            || product === "Wait Time"
            ) {
             if (!isNaN(totalValue)) {
                 totalBilled += totalValue;
