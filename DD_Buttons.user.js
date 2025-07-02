8665bdd5ff8cb570b979b51867cb75054833f981
mtoy30 <mtoy30@gmail.com>
2025-07-02 19:14:43 -0400

    Update DD_Buttons.user.js

--- DD_Buttons.user.js
+++ DD_Buttons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DD_Buttons
 // @namespace    https://github.com/mtoy30/GoTandT
-// @version      3.9.8
+// @version      3.9.9
 // @updateURL   https://raw.githubusercontent.com/mtoy30/GoTandT/main/DD_Buttons.user.js
 // @downloadURL https://raw.githubusercontent.com/mtoy30/GoTandT/main/DD_Buttons.user.js
 // @description  Custom script for Dynamics 365 CRM page with multiple button functionalities
@@ -217,6 +217,7 @@ twoColumnWrapper.appendChild(waitWrapper);
 
     resetButton.onclick = () => {
         input.value = "";
+        waitTimeInput.value = "";
         flatRadio.checked = true;
         mileRadio.checked = false;
         result.innerHTML = "";
