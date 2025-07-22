07bbdef77263be455eb496dc650c903ad934f34f
mtoy30 <mtoy30@gmail.com>
2025-07-22 14:16:15 -0400

    Update DD_Buttons_Admin.user.js

--- DD_Buttons_Admin.user.js
+++ DD_Buttons_Admin.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         DD_Buttons_Admin_TEST
+// @name         DD_Buttons_Admin
 // @namespace    https://github.com/mtoy30/GoTandT
 // @version      4.0.8
-// @updateURL    https://raw.githubusercontent.com/mtoy30/GoTandT/main/DD_Buttons_Admin_TEST.user.js
-// @downloadURL  https://raw.githubusercontent.com/mtoy30/GoTandT/main/DD_Buttons_Admin_TEST.user.js
+// @updateURL    https://raw.githubusercontent.com/mtoy30/GoTandT/main/DD_Buttons_Admin.user.js
+// @downloadURL  https://raw.githubusercontent.com/mtoy30/GoTandT/main/DD_Buttons_Admin.user.js
 // @description  Custom script for Dynamics 365 CRM page with multiple button functionalities
 // @match        https://gotandt.crm.dynamics.com/*
 // @match        https://gttqap2.crm.dynamics.com/*
