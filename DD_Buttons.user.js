c9e17511522b9f96c3d571935f89a3b724ecb080
mtoy30 <mtoy30@gmail.com>
2026-06-30 11:34:30 -0400

    Bump version to 4.1.69 and update modal title

--- DD_Buttons.user.js
+++ DD_Buttons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DD_Buttons
 // @namespace    https://github.com/mtoy30/GoTandT
-// @version      4.1.68
+// @version      4.1.69
 // @updateURL    https://raw.githubusercontent.com/mtoy30/GoTandT/main/DD_Buttons.user.js
 // @downloadURL  https://raw.githubusercontent.com/mtoy30/GoTandT/main/DD_Buttons.user.js
 // @description  Custom script for Dynamics 365 CRM page with multiple button functionalities
@@ -295,7 +295,7 @@ function showLmsNoShowModal() {
         modal.style.fontFamily = "Arial, sans-serif";
 
         const title = document.createElement("div");
-        title.innerText = "Submit No Show to LMS";
+        title.innerText = "Submit Rates to LMS";
         title.style.fontSize = "20px";
         title.style.fontWeight = "bold";
         title.style.marginBottom = "14px";
