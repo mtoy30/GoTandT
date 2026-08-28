c7b1e303467ad228f038ebba9993e02043403bbf
mtoy30 <mtoy30@gmail.com>
2026-08-27 20:03:24 -0400

    Bump version to 4.3.1

--- DD_Buttons_Admin.user.js
+++ DD_Buttons_Admin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DD_Buttons_Admin
 // @namespace    https://github.com/mtoy30/GoTandT
-// @version      4.3.0
+// @version      4.3.1
 // @updateURL    https://raw.githubusercontent.com/mtoy30/GoTandT/main/DD_Buttons_Admin.user.js
 // @downloadURL  https://raw.githubusercontent.com/mtoy30/GoTandT/main/DD_Buttons_Admin.user.js
 // @description  Custom script for Dynamics 365 CRM page with multiple button functionalities
